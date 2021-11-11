f3550078bcdc17ee4b62ff5c37203b067287d84c
c0a3bd <c0a3bd>
2021-11-11 14:42:49 -0500

    completed Introduction to Variables tut

--- Scripts/RaceCar.cs
+++ Scripts/RaceCar.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class RaceCar : MonoBehaviour
 {
+    // declare and initialize variables then use the variables below
+    // vs hard coding the values making the script more dynamic
+    
+    float steerSpeed = 0.1f;
+    float moveSpeed = 0.01f;
+
+
      void Update()
     {
-        transform.Rotate(0, 0, 0.1f);   
+        transform.Rotate(0, 0, steerSpeed);   
         
         // my successful attempt to move my car using only docs
         // transform.Translate(0, Time.deltaTime, 0);
 
         // as recommended in the tut
-        transform.Translate(0, 0.01f, 0);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
 
